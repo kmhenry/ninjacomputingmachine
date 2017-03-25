7a0ec11bed3b084db7d6b8a63b3ac7d67d22cc47
Zac Smith <zrs3141592@gmail.com>
2017-03-25 11:53:19 -0600

    Add faq and contact pages, controller and basic content

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
 
+  get 'faq' => "pages#faq"
+  get 'contact' => "pages#contact"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
